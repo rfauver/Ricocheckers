e3ff8a90553e8f9dae04c80e19053a38beda3f01
Ryan Fauver <ryan.fauver@gmail.com>
2015-04-28 01:07:35 -0400

    create ReinforcementLearnerPlayer

---
+++ src/ReinforcementLearnerPlayer.java
@@ -0,0 +1,16 @@
+
+public class ReinforcementLearnerPlayer extends Player 
+{
+	public int playerNumber;
+	
+	public ReinforcementLearnerPlayer(int p)
+	{
+		playerNumber = p;
+	}
+	
+	public void makeMove(Game g)
+	{
+		super.makeMove(g);
+	}
+	
+}
